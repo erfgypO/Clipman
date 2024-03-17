16337c5931fccd3ac43b4de1de0bcf1770d7d720
Julius Hell <jhell2306@googlemail.com>
2024-03-17 15:01:47 +0100

    Initial Commit

---
+++ Clipman/ClipmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipmanApp.swift
+//  Clipman
+//
+//  Created by Julius Hell on 17.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ClipmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
